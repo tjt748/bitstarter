fab6320ec11f87f1ee254811ad212581f8c6a634
tjt748 <tjt748@yahoo.com>
2013-07-24 13:37:38 +0000

    Updated web.js 7/24/13

--- web.js
+++ web.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var infile = "index.html";
+// var text = fs.readFileSync(infile);
+
+buf = new Buffer();
+buf = fs.readFileSync(infile);
+String text = buf.toString();
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   
-  response.send('Hello World2!');
-});
+  response.send(text);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
